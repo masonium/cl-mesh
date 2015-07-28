facb4caa624c3ea8b89b7da0fe43ccb2b587aff7
Mason Smith <masonium@gmail.com>
2015-07-28 16:33:58 -0700

    type-declarations to vec.lisp for significant speed gain (~3x)

--- vec.lisp
+++ vec.lisp
@@ -6,33 +6,43 @@
   (vector a b c))
 
 (defun v+ (a b)
+  (declare (type (simple-vector 3) a b))
   (map 'vector #'+ a b))
 
 (defun v- (a b)
+  (declare (type (simple-vector 3) a b))
   (map 'vector #'- a b))
 
 (defun v-norm2 (a)
+  (declare (type (simple-vector 3) a))
   (v-dot a a))
 
 (defun v-norm (a)
+  (declare (type (simple-vector 3) a))
   (sqrt (v-norm2 a)))
 
 (defun v-cos-angle (a b)
+  (declare (type (simple-vector 3) a b))
   (/ (v-dot a b) (v-norm a) (v-norm b)))
 
 (defun v-dist2 (a b)
+  (declare (type (simple-vector 3) a b))
   (v-norm2 (v- a b)))
 
 (defun v*f (a f)
+  (declare (type (simple-vector 3) a))
   (map 'vector #'(lambda (x) (* x f)) a))
 
 (defun v-normal (a)
+  (declare (type (simple-vector 3) a))
   (v*f a (/ (v-norm a))))
 
 (defun v-dot (a b)
+  (declare (type (simple-vector 3) a b))
   (reduce #'+ (map 'vector #'* a b)))
 
 (defun v-cross (a b)
+  (declare (type (simple-vector 3) a b))
   (let+ ((#(x1 x2 x3) a)
          (#(y1 y2 y3) b))
     (v3 (- (* x2 y3) (* x3 y2))
